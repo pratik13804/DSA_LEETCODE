9a6a6651fcf2026bab24f1f2bec174a093371b32
pratik13804 <pratik13804pp@gmail.com>
2026-07-05 18:23:20 +0530

    Time: 0 ms (100.00%) | Memory: 118 MB (65.53%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(!head||!head->next) return true;
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(fast&&fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode*prev=nullptr;
+        ListNode*curr=slow;
+        while(curr){
+            ListNode*nextNode=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nextNode;
+        }
+        ListNode*firsthalf=head;
+        ListNode*secondhalf=prev;
+        bool isPalin=true;
+        while(secondhalf){
+            if(firsthalf->val!=secondhalf->val){
+                isPalin=false;
+                break;
+            }
+            firsthalf=firsthalf->next;
+            secondhalf=secondhalf->next;
+
+        }
+        return isPalin;
+    }
+};
